1e17fcb9a66cb6569ecf5a337c8a6a5092377b7f
Nicolas Traczuk <ntraczuk92@gmail.com>
2025-03-28 10:14:42 -0300

    transitions2

--- lib/project_social_networks_web/live/thougth_live.ex
+++ lib/project_social_networks_web/live/thougth_live.ex
@@ -126,7 +126,7 @@ defmodule ProjectSocialNetworksWeb.ThoughtLive do
         <!-- Botón para abrir modal -->
         <button id="abrir-modal"
           phx-click="mostrar_modal"
-          class="transform transition duration-300 ease-in-out fixed bottom-8 right-8 bg-red-300 hover:bg-red-400 text-white rounded-full p-4 shadow-lg"
+          class="transform transition duration-300 ease-in-out fixed bottom-8 right-8 bg-red-300 hover:bg-red-400 text-white rounded-full p-4 shadow-lg hover:cursor-pointer"
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
@@ -162,13 +162,13 @@ defmodule ProjectSocialNetworksWeb.ThoughtLive do
                     </div>
                   <% end %>
                   <div class="mt-3 flex space-x-4 text-gray-500 text-sm">
-                    <button class="transform transition duration-300 ease-in-out flex items-center space-x-1 hover:text-white hover:bg-blue-400 rounded-full p-2">
+                    <button class="transform transition duration-300 ease-in-out flex items-center space-x-1 hover:text-white hover:bg-blue-400 rounded-full p-2 hover:cursor-pointer">
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                       </svg>
                       <span>Like</span>
                     </button>
-                    <button class="transform transition duration-300 ease-in-out flex items-center space-x-1 hover:text-white hover:bg-green-400 rounded-full p-2">
+                    <button class="transform transition duration-300 ease-in-out flex items-center space-x-1 hover:text-white hover:bg-green-400 rounded-full p-2 hover:cursor-pointer">
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                       </svg>
