719a6f0fdee38d3a5baeba8b794c4b1e61664875
Nicolas Traczuk <ntraczuk92@gmail.com>
2025-03-28 09:44:53 -0300

    transitions

--- lib/project_social_networks_web/live/thougth_live.ex
+++ lib/project_social_networks_web/live/thougth_live.ex
@@ -182,7 +182,8 @@ defmodule ProjectSocialNetworksWeb.ThoughtLive do
         </div>
       </div>
 
-      <!-- Modal -->
+
+
       <%= if @mostrar_modal do %>
         <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
             phx-window-keydown="cerrar_modal"
